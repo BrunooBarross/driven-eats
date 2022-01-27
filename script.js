3174372acd442e7f00973cbc4c1f5f338dfbc59e
Bruno Barros <barrosbrunofelipe@gmail.com>
2022-01-27 15:36:51 -0300

    adicionando arquivo js e funções para escolha dos pratos

---
+++ script.js
@@ -0,0 +1,30 @@
+function escolherFrango(){   
+    document.getElementById("frango").style.borderColor = "green";
+    document.getElementById("sushi").style.borderColor = "white";
+    document.getElementById("peixe").style.borderColor = "white";
+}
+function escolherSushi(){
+    document.getElementById("sushi").style.borderColor = "green";
+    document.getElementById("peixe").style.borderColor = "white";   
+    document.getElementById("frango").style.borderColor = "white";
+}
+function escolherPeixe(){
+    document.getElementById("peixe").style.borderColor = "green";
+    document.getElementById("sushi").style.borderColor = "white";
+    document.getElementById("frango").style.borderColor = "white";
+}
+function escolherCoca(){   
+    document.getElementById("coca").style.borderColor = "green";
+    document.getElementById("fanta").style.borderColor = "white";
+    document.getElementById("heineken").style.borderColor = "white";
+}
+function escolherFanta(){
+    document.getElementById("fanta").style.borderColor = "green";
+    document.getElementById("coca").style.borderColor = "white";   
+    document.getElementById("heineken").style.borderColor = "white";
+}
+function escolherHeineken(){
+    document.getElementById("heineken").style.borderColor = "green";
+    document.getElementById("coca").style.borderColor = "white";
+    document.getElementById("fanta").style.borderColor = "white";
+}
