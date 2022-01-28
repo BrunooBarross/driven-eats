87aa7306c0beba9df1f324a13da4edb885889f14
Bruno Barros <barrosbrunofelipe@gmail.com>
2022-01-27 21:05:48 -0300

    tirando o modal teste

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let prato = "";
-let bebida = "";
-let sobremesa ="";
+let prato;
+let bebida;
+let sobremesa;
 let valorPrato;
 let valorBebida;
 let valorSobremesa;
@@ -10,7 +10,7 @@ document.querySelector("#botao").disabled=true;
 
 
 function habilitarBotao(){  
-    if(prato !== "" && bebida !== "" && sobremesa !== ""){  
+    if(prato !== undefined && bebida !== undefined && sobremesa !== undefined){  
         document.querySelector("#botao").disabled=false;      
         document.getElementById("botao").style.background = "green";
         document.getElementById("botao").innerHTML="<p>Finalizar pedido</p>";
@@ -97,28 +97,3 @@ function finalizarPedido(){
 }
 
 
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
